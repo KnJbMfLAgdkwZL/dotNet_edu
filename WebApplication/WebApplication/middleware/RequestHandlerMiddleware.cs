989a50ba36b3333b22a5749a057d07b3461523f8
KnJbMfLAgdkwZL <zipocat@gmail.com>
2021-07-01 00:42:56 +0300

    pizdos

---
+++ WebApplication/WebApplication/middleware/RequestHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace WebApplication.middleware
+{
+    public class RequestHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+            Console.WriteLine("Request");
+            var method = context.Request.Method;
+            var url = context.Request.GetDisplayUrl();
+            var protocol = context.Request.Protocol;
+            Console.WriteLine($"{method} {url} {protocol}");
+            foreach (var (key, value) in context.Request.Headers)
+                Console.WriteLine($"\t{key}: {value}");
+            using (var reader
+                = new StreamReader(context.Request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                var bodyStr = await reader.ReadToEndAsync();
+                if (bodyStr.Length > 0)
+                    Console.WriteLine($"Body: {bodyStr}");
+                context.Request.Body.Position = 0;
+            }
+
+            Console.WriteLine();
+            await _next(context);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/middleware/ResponseHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication.middleware
+{
+    public class ResponseHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ResponseHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var responseBody = "";
+            await using (var swapStream = new MemoryStream())
+            {
+                var originalResponseBody = context.Response.Body;
+                context.Response.Body = swapStream;
+                await _next(context);
+                swapStream.Seek(0, SeekOrigin.Begin);
+                responseBody = new StreamReader(swapStream).ReadToEnd();
+                swapStream.Seek(0, SeekOrigin.Begin);
+                await swapStream.CopyToAsync(originalResponseBody);
+                context.Response.Body = originalResponseBody;
+            }
+
+            Console.WriteLine("Response");
+            var statusCode = context.Response.StatusCode;
+            Console.WriteLine($"StatusCode: {statusCode}");
+            foreach (var (key, value) in context.Response.Headers)
+                Console.WriteLine($"\t{key}: {value}");
+            if (responseBody.Length > 0)
+                Console.WriteLine($"ResponseBody: {responseBody}");
+            Console.WriteLine();
+        }
+    }
+}
